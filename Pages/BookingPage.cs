9d02face697cfbd0217c1b8f709e60ed2a46efd2
0x0null <45559731+0x0null@users.noreply.github.com>
2025-04-24 11:42:02 +0100

    removed redundant code

--- Pages/BookingPage.cs
+++ Pages/BookingPage.cs
@@ -17,19 +17,6 @@ public class BookingPage : BasePage
 
     public async Task ClickNextAsync() => await Page.GetByRole(AriaRole.Button, new() { Name = "Next 16:" }).ClickAsync();
 
-
-    public async Task FillFormAsync()
-    {
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Name *" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Name *" }).FillAsync("Cal");
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Name *" }).PressAsync("Tab");
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Email *" }).FillAsync("cal@cal.com");
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Company *" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Company *" }).FillAsync("cal ng ltd");
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Topic description" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Topic description" }).FillAsync("na");
-    }
-
     public async Task ScheduleEventAsync() => await Page.GetByRole(AriaRole.Button, new() { Name = submitButton }).ClickAsync();
 
     public async Task ValidateBookingAsync() => await assert.Expect(Page.GetByText("You are scheduled", new() { Exact = true })).ToBeVisibleAsync();
